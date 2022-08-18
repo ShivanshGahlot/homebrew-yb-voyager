a7b0ddc4bd6248ecc3fd0c8c97189b57030eb262
Shivansh Gahlot <42472145+ShivanshGahlot@users.noreply.github.com>
2022-08-18 16:59:09 +0530

    Update yb-voyager.rb

--- yb-voyager.rb
+++ yb-voyager.rb
@@ -3,14 +3,14 @@ class YbVoyager < Formula
   homepage "https://github.com/yugabyte/yb-voyager/"
   url "https://github.com/yugabyte/yb-voyager/archive/refs/tags/yb-voyager/v1.0.0-xbeta.0.tar.gz"
   sha256 "f861453ff4b87c05cad5660dbd6997c8b758e4d9235b49b582937f6aee917c90"
-  license "ASL 2.0"
+  license "Apache-2.0"
 
   depends_on "go" => :build
   depends_on "libpq"
 
   def install
-    ENV.deparallelize  # if your formula fails when building in parallel
-    Dir.chdir('yb-voyager')
+    ENV.deparallelize
+    Dir.chdir("yb-voyager")
     system "go", "build"
     bin.install "yb-voyager"
   end
@@ -18,4 +18,3 @@ class YbVoyager < Formula
   test do
     system "#{bin}/yb-voyager", "--help"
   end
-end
