91ea1beebdc45712d96d4851126e4b178e721642
Shivansh Gahlot <42472145+ShivanshGahlot@users.noreply.github.com>
2022-08-19 17:21:44 +0530

    Update yb-voyager.rb

--- yb-voyager.rb
+++ yb-voyager.rb
@@ -4,7 +4,7 @@ class YbVoyager < Formula
   url "https://github.com/yugabyte/yb-voyager/archive/refs/tags/yb-voyager/v1.0.0-xbeta.0.tar.gz"
   sha256 "f861453ff4b87c05cad5660dbd6997c8b758e4d9235b49b582937f6aee917c90"
   license "Apache-2.0"
-  version 2.0
+  version 1.0
 
   depends_on "go" => :build
   depends_on "libpq"
