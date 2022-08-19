67a5d87f1283b60a301c413929de0751f5e38b4c
Shivansh Gahlot <42472145+ShivanshGahlot@users.noreply.github.com>
2022-08-19 17:40:51 +0530

    Update yb-voyager.rb

--- yb-voyager.rb
+++ yb-voyager.rb
@@ -4,16 +4,17 @@ class YbVoyager < Formula
   url "https://github.com/yugabyte/yb-voyager/archive/refs/tags/yb-voyager/v1.0.0-xbeta.0.tar.gz"
   sha256 "f861453ff4b87c05cad5660dbd6997c8b758e4d9235b49b582937f6aee917c90"
   license "Apache-2.0"
-  version "1.0"
+  version "2.0"
 
   depends_on "go" => :build
   depends_on "libpq"
 
   def install
     ENV.deparallelize
-    Dir.chdir("yb-voyager")
-    system "go", "build"
-    bin.install "yb-voyager"
+    Dir.chdir("yb-voyager") do
+      system "go", "build"
+      bin.install "yb-voyager"
+    end
   end
 
   test do
